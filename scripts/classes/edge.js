d9813de025d6785b28e06c66b1ee162b03fa31fc
Raymon Zutekouw <raymonzutekouw@gmail.com>
2018-11-11 18:07:11 +0100

    Added the weight onto the edge as text   * Moves along with the edge

--- scripts/classes/edge.js
+++ scripts/classes/edge.js
@@ -1,12 +1,30 @@
 class Edge {
-  constructor(node1, node2) {
+  constructor(node1, node2, weight) {
     this.node1 = node1;
     this.node2 = node2;
+    this.weight = weight;
+    this.relCenter = this.getRelCenter();
+    this.text_offsetY = -height / 30;
   }
 
   draw() {
     stroke(0);
     strokeWeight(height / 125);
     line(this.node1.x, this.node1.y, this.node2.x, this.node2.y);
+
+    fill(255);
+    strokeWeight(3);
+    this.relCenter = this.getRelCenter();
+    textSize(height / 20);
+    text(this.weight, this.relCenter['x'], this.relCenter['y'] + this.text_offsetY);
+  }
+
+  getRelCenter() {
+    let relX_center = (this.node1.x + this.node2.x) / 2;
+    let relY_center = (this.node1.y + this.node2.y) / 2;
+    return {
+      x: relX_center,
+      y: relY_center
+    }
   }
 }
