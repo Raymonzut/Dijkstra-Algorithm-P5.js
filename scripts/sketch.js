5d762503495ad9daa3a6bf2764984a456d4779b2
Raymon Zutekouw <raymonzutekouw@gmail.com>
2018-11-14 21:20:39 +0100

    Renamed the add tools and made the add tools   * Nodes can be moved, edges and weight move with them   * Multiple edges per node is possible

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,13 +1,19 @@
 let nodes = [];
 let edges = [];
+let selectedNodes = [];
 let nodeRadius;
 let selected_node_index;
+let selectedNodesIndexes = [];
 let isGrabbing = false;
 let settings = {
   modes: [],
   states: []
 };
 
+
+/**
+ * Loads the different modes that are specified in the html under the options div
+ */
 function setModes() {
   let options = document.getElementsByClassName('option');
   for (var i = 0; i < options.length - 1; i++) {
@@ -16,6 +22,9 @@ function setModes() {
   }
 }
 
+/**
+ * Checks which options are checked
+ */
 function checkModes() {
   let options = document.getElementsByClassName('option');
   for (var i = 0; i < options.length - 1; i++) {
@@ -28,18 +37,19 @@ function setup() {
   createCanvas(750, 750);
   nodeRadius = height / 12;
 
-  // Add the Nodes
+  // Adds a begin graph, with 2 nodes and a edge to connect them
   nodes.push(new Node(100, 100, nodeRadius));
   nodes.push(new Node(600, 200, nodeRadius));
-  edges.push([new Edge(nodes[0], nodes[1], 10), 0, 1]);
+  edges.push([new Edge(nodes[0], nodes[1], '?'), 0, 1]);
+
   setModes();
-  checkModes();
 
   textAlign(CENTER, CENTER);
 }
 
 function draw() {
   background(133);
+
   // Draw all elements that can be drawn
   for (var i = 0; i < edges.length; i++) {
     edges[i][0].draw()
@@ -47,13 +57,17 @@ function draw() {
   for (var i = 0; i < nodes.length; i++) {
     nodes[i].draw()
   }
-  checkModes();
+
 }
 
+/**
+ * Sets selected_node_index to the node where the mouse is on
+ * selected_node_index will be null when the mouse is not on a node
+ */
 function checkOnNode() {
   let mousePos = [mouseX, mouseY];
   let broke_out = false;
-  for (var i = 0; i <= nodes.length - 1; i++) {
+  for (var i = 0; i < nodes.length; i++) {
     if (((nodes[i].x - nodeRadius) < mousePos[0] && mousePos[0] < (nodes[i].x + nodeRadius)) && ((nodes[i].y - nodeRadius) < mousePos[1] && mousePos[1] < (nodes[i].y + nodeRadius))) {
       selected_node_index = i;
       broke_out = true;
@@ -65,15 +79,32 @@ function checkOnNode() {
   }
 }
 
+/**
+ * Will grab the node where the mouse is on, this node will be replaced with the update node:
+ * It will have a new x and y position
+ * The edges [] will be updated with the new node
+ */
 function replaceNode() {
+  checkOnNode();
+  let mousePos = [mouseX, mouseY];
+
+  // TODO: Fix the edge not moving with bug
   if (selected_node_index != null) {
-    nodes[selected_node_index] = null;
-    nodes[selected_node_index] = new Node(mouseX, mouseY, nodeRadius);
+    // Replace the node
+    nodes[selected_node_index] = new Node(mousePos[0], mousePos[1], nodeRadius);
+
+    let movingNode = nodes[selected_node_index];
+
     for (var i = 0; i < edges.length; i++) {
-      if (edges[i][1] == selected_node_index) {
-        edges[i] = [new Edge(nodes[0], nodes[1], edges[i][0].weight), selected_node_index, edges[i][2]];
-      } else if (edges[i][2] == selected_node_index) {
-        edges[i] = [new Edge(nodes[0], nodes[1], edges[i][0].weight), edges[i][1], selected_node_index];
+      // No loops (yet)
+      // An edge in the edge array: [edge(node1, node2, weight), index1, index2]
+      if (edges[i][1] != edges[i][2]) {
+        if (edges[i][1] == selected_node_index) {
+          edges[i] = [new Edge(movingNode, edges[i][0].node2, edges[i][0].weight), selected_node_index, edges[i][2]];
+        } else if (edges[i][2] == selected_node_index) {
+          edges[i] = [new Edge(edges[i][0].node1, movingNode, edges[i][0].weight), edges[i][1], selected_node_index];
+        }
+
       }
     }
   }
@@ -81,14 +112,43 @@ function replaceNode() {
 }
 
 function mousePressed() {
+  checkModes();
   isGrabbing = !isGrabbing;
-  checkOnNode(settings.modes);
+  checkOnNode();
+
+  // Checks if add node is selected
+  if (settings["states"][settings["modes"].indexOf("addNodeTool")]) {
+    let mousePos = [mouseX, mouseY];
+    if ((0 < mousePos[0] && mousePos[0] < width) && (0 < mousePos[1] && mousePos[1] < height)) {
+      nodes.push(new Node(mousePos[0], mousePos[1], nodeRadius));
+    }
+  }
+
+  if (settings["states"][settings["modes"].indexOf("addEdgeTool")]) {
+    let node1, node2;
+
+    // No selected edges
+    if (selectedNodes.length == 0 && selected_node_index != undefined) {
+      node1 = nodes[selected_node_index];
+      selectedNodes.push(node1);
+      selectedNodesIndexes.push(nodes.indexOf(selectedNodes[0]));
+
+    } else if (selectedNodes.length == 1 && selected_node_index != undefined) {
+      node2 = nodes[selected_node_index];
+      selectedNodes.push(node2);
+      selectedNodesIndexes.push(nodes.indexOf(selectedNodes[1]));
+      // Now, we have 2 selectedNodes
+      edges.push([new Edge(nodes[selectedNodesIndexes[0]], nodes[selectedNodesIndexes[1]], '?'), selectedNodesIndexes[0], selectedNodesIndexes[1]]);
+      selectedNodesIndexes = [];
+      selectedNodes = [];
+    }
+  }
+
 }
 
 function mouseMoved() {
   // Checks if grabbing and checks if the grab node button is on
   if (isGrabbing && settings["states"][settings["modes"].indexOf("grabTool")]) {
-    checkOnNode();
     replaceNode();
   }
 
