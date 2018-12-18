c2a9af501a4121c2cea2b75a3a763e76ba43685d
Raymon Zutekouw <raymonzutekouw@gmail.com>
2018-12-18 21:22:43 +0100

    Added updateNeighbours Refactored getLowestNodeIndex with stopping condition Added calculate interval

--- scripts/path.js
+++ scripts/path.js
@@ -15,46 +15,97 @@ function showRoute() {
 
 }
 
+function updateNeighbours(index) {
+  for (var i = 0; i < edges.length; i++) {
+    if (edges[i].connected.includes(index)) {
+
+      let node_index_here = edges[i].getCurrentSideIndex(index);
+      let node_index_there = edges[i].getOtherSideIndex(index);
+
+      nodes[node_index_there].color = color(100, 100, 100);
+
+      let current_neighbour_weight = nodes[node_index_there].weight;
+      let new_neighbour_weight = nodes[node_index_here].weight + parseInt(edges[i].weight);
+
+      console.log(`${current_neighbour_weight}, ${new_neighbour_weight}`);
+
+      if (current_neighbour_weight > new_neighbour_weight) {
+        nodes[node_index_there].weight = new_neighbour_weight;
+      }
+    }
+  }
+}
+
+
 /**
  * Will update weights of nodes
  * Will return the index of the lowest node
  */
-// TODO: Fix the weight setting to nodes
-function getLowestNeighbour(index) {
-  // Find neighbours, adds it as [index, edgeWeight]
-  let neighbourIndexes = [];
-  for (var i = 1; i < edges.length; i++) {
-    if (index == edges[i].index1) {
-      neighbourIndexes.push([edges[i].index1, edges[i].weight]);
-    } else if (index == edges[i].index2) {
-      neighbourIndexes.push([edges[i].index2, edges[i].weight]);
-    }
-  }
-  console.log(neighbourIndexes);
-  // Update weights
-  for (var i = 0; i < neighbourIndexes.length; i++) {
-    if (parseInt(nodes[neighbourIndexes[i][0]].weight) > parseInt(nodes[index].weight) + neighbourIndexes[i][1]) {
-      nodes[neighbourIndexes[i][0]].weight = (parseInt(nodes[index].weight) + neighbourIndexes[i][1]).toString();
-      console.log(nodes[neighbourIndexes[i][0]].weight);
-    }
-  }
+// Check if valid, haven't seen mistake (yet)
+function getLowestNodeIndex(endIndex) {
+  let lowestIndex;
+  let changed = false;
 
-  // get the lowest neighbourIndex
-  let lowestIndex = 0
-  for (var i = 1; i < neighbourIndexes.length; i++) {
-    if (parseInt(nodes[lowestIndex].weight) < parseInt(nodes[i].weight)) {
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].canVisit) {
       lowestIndex = i;
     }
   }
-  return lowestIndex;
+
+  for (var i = 0; i < nodes.length; i++) {
+    if ((nodes[lowestIndex].weight >= nodes[i].weight) && nodes[i].canVisit) {
+      changed = true;
+      lowestIndex = i;
+    }
+  }
+
+  if (!changed) {
+    console.log("No lower index");
+    return null;
+  } else if (lowestIndex == endIndex) {
+    console.log("End reached early");
+    return null;
+  } else {
+    return lowestIndex;
+  }
+
 }
+
 
 /**
  * Will use Dijkstra's algorithm to calculate the shortest route
- * Will move a 'player' by setting is_current to the lowest weighted node
+ * Will move a 'player' by setting currentIndex to the lowest weighted node
  */
 function calculateRoute() {
+  let currentIndex;
+  let endIndex;
+
+  let canExit = false;
   for (var i = 0; i < nodes.length; i++) {
     nodes[i].initPathFinding();
+    if (nodes[i].is_start) {
+      currentIndex = i;
+    }
+    if (nodes[i].is_end) {
+      endIndex = i;
+    }
   }
+
+  // Reconsider ending condition
+  // Algoritm not "Walking" properly
+  let timer = setInterval(function() {
+    if (canExit) {
+      clearInterval(timer);
+      return;
+    }
+    updateNeighbours(currentIndex);
+    nodes[currentIndex].canVisit = false;
+    currentIndex = getLowestNodeIndex(endIndex);
+    canExit = (currentIndex == null);
+  }, 2000);
+
+  console.log(currentIndex == endIndex);
+
+
+
 }
